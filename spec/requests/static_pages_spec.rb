9241b4093c737b59d2827eb527a42bf39a27653d
BabyCatherine <26katya91@gmail.com>
2016-12-01 15:33:56 +0300

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,8 +29,8 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    it { should have_content('Contact') } # it { should have_selector('h1', text: 'Contact') }   5.6
+    it { should have_title(full_title('Contact')) }  # it { should have_title(full_title('Contact')) }
   end
 end    
 
